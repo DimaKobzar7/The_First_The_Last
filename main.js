4cf705213819ec55f654d066d3fb67324bd2b2d1
Dima Kobzar <kobzar7dima@gmail.com>
2022-12-08 21:49:37 +0200

    lang animation done

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
 const langIco = document.querySelector('.intro__language')
 const langLinks = document.querySelectorAll('.intro__switch-link')
+const rulang = document.querySelector('.intro__switch-link--RU')
+const uaLang = document.querySelector('.intro__switch-link--UA')
 
 langIco.addEventListener('mouseover', (event) => {
-  langLinks.forEach(item => {
-    // item.classList.remove('testHover');
-    // item.style.setProperty("color", "red","important")
-    item.style.setProperty("opacity", "1", )
-    item.style.setProperty("transition", "left 3s, opacity 3s")
-    // item.classList.add('testTransition')
-    console.log(item);
-  })
+
+    rulang.style.setProperty("opacity", "1", )
+    rulang.style.setProperty("transition", "left 1s, opacity 2s")
+    // console.log(item);
+
+    uaLang.style.setProperty("opacity", "1", )
+    uaLang.style.setProperty("transition", "left 1s, opacity 2s")
 });
 
 
 langIco.addEventListener('mouseout', (event) => {
-  // console.log(langLinks);
-  langLinks.forEach(item => {
-    // item.classList.remove('testHover');
-    // item.style.setProperty("color", "red","important")
-    item.style.setProperty("opacity", "0")
-    // item.style.setProperty("transition", "left 10s")
-
-
-    // item.classList.add('testTransition')
-    console.log(item);
-  })
+  uaLang.style.setProperty("opacity", "0")
+  rulang.style.setProperty("opacity", "0",)
+  rulang.style.setProperty("transition", "left 1s, opacity 2s")
+  uaLang.style.setProperty("transition", "left 2s, opacity 2.5s")
 });
+
+// langIco.addEventListener('mouseover', (event) => {
+//   langLinks.forEach(item => {
+//     item.style.setProperty("opacity", "1", )
+//     item.style.setProperty("transition", "left 3s, opacity 3s")
+//     console.log(item);
+//   })
+// });
+
+
+// langIco.addEventListener('mouseout', (event) => {
+//   langLinks.forEach(item => {
+//     item.style.setProperty("opacity", "0")
+
+//     console.log(item);
+//   })
+// });
