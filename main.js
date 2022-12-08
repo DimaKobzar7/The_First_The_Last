2f76f7073dfafd17d39f09708684025e4c978ddb
Dima Kobzar <kobzar7dima@gmail.com>
2022-12-08 23:11:40 +0200

    title animation smooth animation but not done

--- main.js
+++ main.js
@@ -68,12 +68,12 @@ spiner.addEventListener('mouseout', (event) => {
 
 (function () {
 
-  const link = document.querySelector('.intro__title');
+  const link = document.querySelector('main');
   // const cursor = document.querySelector('.cursor');
 
   const animateit = function (e) {
-        const span = document.querySelector('h1');
-        console.log(span);
+        const title = document.querySelector('.intro__title');
+        console.log(title);
         const { offsetX: x, offsetY: y } = e,
         { offsetWidth: width, offsetHeight: height } = this,
 
@@ -81,10 +81,10 @@ spiner.addEventListener('mouseout', (event) => {
         xMove = x / width * (move * 2) - move,
         yMove = y / height * (move * 2) - move;
 
-        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+        title.style.transform = `translate(${xMove}px, ${yMove}px)`;
 
         if (e.type === 'mouseleave') {
-          span.style.transform = '';
+          title.style.transform = '';
         }
   };
 
@@ -99,3 +99,114 @@ spiner.addEventListener('mouseout', (event) => {
   // window.addEventListener('mousemove', editCursor);
 
 })();
+
+// (function () {
+
+//   const link = document.querySelectorAll('nav > .hover-this');
+//   const cursor = document.querySelector('.cursor');
+
+//   const animateit = function (e) {
+//         const span = this.querySelector('span');
+//         const { offsetX: x, offsetY: y } = e,
+//         { offsetWidth: width, offsetHeight: height } = this,
+
+//         move = 25,
+//         xMove = x / width * (move * 2) - move,
+//         yMove = y / height * (move * 2) - move;
+
+//         span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+//         if (e.type === 'mouseleave') span.style.transform = '';
+//   };
+
+//   const editCursor = e => {
+//         const { clientX: x, clientY: y } = e;
+//         cursor.style.left = x + 'px';
+//         cursor.style.top = y + 'px';
+//   };
+
+//   link.forEach(b => b.addEventListener('mousemove', animateit));
+//   link.forEach(b => b.addEventListener('mouseleave', animateit));
+//   window.addEventListener('mousemove', editCursor);
+
+// })();
+
+// HTML
+{/* <div class="nav-wrapper">
+  <nav>
+    <a href="#" class="hover-this"><span>Home</span></a>
+    <a href="#" class="hover-this"><span>Our Story</span></a>
+    <a href="#" class="hover-this"><span>Studio</span></a>
+    <a href="#" class="hover-this"><span>Contact</span></a>
+    <div class="cursor"></div>
+  </nav>
+</div> */}
+
+// CSS
+// html, body {
+//   margin: 0;
+//   padding: 0;
+//   cursor: none;
+// }
+
+// .nav-wrapper {
+//   width: 100%;
+//   height: 100vh;
+//   background: #161616;
+// }
+
+// nav {
+//   width: 100%;
+//   margin: 0 auto;
+//   text-align: center;
+//   position: absolute;
+//   top: 50%;
+// }
+
+// .hover-this {
+//   transition: all 0.3s ease;
+// }
+
+// span {
+//   display: inline-block;
+//   font-family: 'Russo One', sans-serif;
+//   font-weight: 400;
+//   color: #fff;
+//   font-size: 36px;
+//   text-transform: uppercase;
+//   pointer-event: none;
+//   transition: transform 0.1s linear;
+// }
+
+// .cursor {
+//   pointer-events: none;
+//   position: fixed;
+//   padding: 0.3rem;
+//   background-color: #fff;
+//   border-radius: 50%;
+//   mix-blend-mode: difference;
+//   transition: transform 0.3s ease;
+// }
+
+// .hover-this:hover ~ .cursor {
+//   transform:translate(-50%, -50%) scale(8);
+// }
+
+// @media(min-width: 900px) {
+//   nav {
+//     display: flex;
+//     justify-content: space-around;
+//   }
+// }
+
+// @media(max-width: 900px) {
+//   nav {
+//     top: 30%;
+//   }
+
+//   .hover-this {
+//     width: 100%;
+//     padding: 20px 0;
+//     display: inline-block;
+//   }
+// }
