f42a2ec2e94b4959fa0ef6d690167c6403efdb83
Dima Kobzar <kobzar7dima@gmail.com>
2022-12-08 22:57:53 +0200

    title animation start

--- main.js
+++ main.js
@@ -64,3 +64,38 @@ spiner.addEventListener('mouseout', (event) => {
   // spiner.style.setProperty("transition", "transform 3s")
   spinerSvg.style.setProperty("animation", "spin 5s infinite linear paused")
 });
+
+
+(function () {
+
+  const link = document.querySelector('.intro__title');
+  // const cursor = document.querySelector('.cursor');
+
+  const animateit = function (e) {
+        const span = document.querySelector('h1');
+        console.log(span);
+        const { offsetX: x, offsetY: y } = e,
+        { offsetWidth: width, offsetHeight: height } = this,
+
+        move = 25,
+        xMove = x / width * (move * 2) - move,
+        yMove = y / height * (move * 2) - move;
+
+        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+        if (e.type === 'mouseleave') {
+          span.style.transform = '';
+        }
+  };
+
+  // const editCursor = e => {
+  //       const { clientX: x, clientY: y } = e;
+  //       cursor.style.left = x + 'px';
+  //       cursor.style.top = y + 'px';
+  // };
+
+  link.addEventListener('mousemove', animateit);
+  link.addEventListener('mouseleave', animateit);
+  // window.addEventListener('mousemove', editCursor);
+
+})();
